313c4e29b65566d8c8cd04eb9195e0b7402c574e
nizmkm <nizammusthafa815@gmail.com>
2022-06-11 11:34:44 +0530

    greater

--- greater.py
+++ greater.py
@@ -1,11 +1,8 @@
 a=int(input('first number'))
 b=int(input('second number'))
 c=int(input('third number'))
-if a>b:
-    if a>c:
-        print(a,'is greater')
-    else:
-        print(c,'is greater')
+if a>b and a>c:
+    print(a,'is greater')
 elif b>c :
     print(b,'is greater')
 else:
